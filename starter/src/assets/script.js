55e80a1d8e5b5b5c11eac9dae337d8c19c0a35f3
Saddamzy001 <saddamhusseini1234@gmail.com>
2023-12-03 01:05:31 +0100

    making updates

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -1,145 +1,149 @@
-// Create an array named products to hold product objects
+/* Create an array named products which you will use to add all of your product object literals that you create in the next step. */
 const products = [
   {
-    name: 'Cherry',
-    price: 2.5,
+    name: "Cherry",
+    price: 2,
     quantity: 0,
     productId: 1,
-    image: 'images/cherry.jpg',
+    image: "./images/cherry.jpg",
   },
   {
-    name: 'Orange',
-    price: 1.8,
+    name: "Orange",
+    price: 3,
     quantity: 0,
     productId: 2,
-    image: 'images/orange.jpg',
+    image: "./images/orange.jpg",
   },
   {
-    name: 'Strawberry',
-    price: 3.2,
+    name: "Strawberry",
+    price: 1.5,
     quantity: 0,
     productId: 3,
-    image: 'images/strawberry.jpg',
+    image: "./images/strawberry.jpg",
   },
 ];
 
-// Declare an empty array named cart to hold the items in the cart
+/* Create 3 or more product objects using object literal notation 
+   Each product should include five properties
+   - name: name of product (string)
+   - price: price of product (number)
+   - quantity: quantity in cart should start at zero (number)
+   - productId: unique id for the product (number)
+   - image: picture of product (url string)
+*/
+/* Images provided in /images folder. All images from Unsplash.com
+   - cherry.jpg by Mae Mu
+   - orange.jpg by Mae Mu
+   - strawberry.jpg by Allec Gomes
+*/
+
+/* Declare an empty array named cart to hold the items in the cart */
 const cart = [];
 
-// Create a global variable for totalPaid
-let totalPaid = 0;
-
-// Create a function named addProductToCart
+/* Create a function named addProductToCart that takes in the product productId as an argument
+  - addProductToCart should get the correct product based on the productId
+  - addProductToCart should then increase the product's quantity
+  - if the product is not already in the cart, add it to the cart
+*/
 function addProductToCart(productId) {
-  const product = products.find((item) => item.productId === productId);
-
-  if (!product) {
-    console.error('Product not found');
-    return;
-  }
-
-  const cartItem = cart.find((item) => item.productId === productId);
-
-  if (cartItem) {
-    // Product is already in the cart, increase quantity
-    cartItem.quantity++;
+  const product = products.find((product) => product.productId === productId);
+  if (cart.includes(product)) {
+    product.quantity += 1;
   } else {
-    // Product is not in the cart, add it
-    cart.push({ ...product, quantity: 1 });
+    product.quantity = 1;
+    cart.push(product);
   }
 }
 
-// Create a function named increaseQuantity
+/* Create a function named increaseQuantity that takes in the productId as an argument
+  - increaseQuantity should get the correct product based on the productId
+  - increaseQuantity should then increase the product's quantity
+*/
 function increaseQuantity(productId) {
-  const cartItem = cart.find((item) => item.productId === productId);
-
-  if (cartItem) {
-    // Increase the product's quantity
-    cartItem.quantity++;
-  } else {
-    console.error('Product not found in the cart');
-  }
-}
-
-// Create a function named decreaseQuantity
-function decreaseQuantity(productId) {
-  const cartItem = cart.find((item) => item.productId === productId);
-
-  if (cartItem) {
-    // Decrease the product's quantity
-    cartItem.quantity--;
-
-    if (cartItem.quantity <= 0) {
-      // If quantity reaches 0, remove the product from the cart
-      removeProductFromCart(productId);
+  cart.forEach((product) => {
+    if (productId === product.productId) {
+      product.quantity++;
     }
-  } else {
-    console.error('Product not found in the cart');
-  }
+  });
 }
-
-// Create a function named removeProductFromCart
+/* Create a function named decreaseQuantity that takes in the productId as an argument
+  - decreaseQuantity should get the correct product based on the productId
+  - decreaseQuantity should decrease the quantity of the product
+  - if the function decreases the quantity to 0, the product is removed from the cart
+*/
+function decreaseQuantity(productId) {
+  cart.forEach((product, index) => {
+    if (productId === product.productId) {
+      if (product.quantity === 1) {
+        cart.splice(index, 1);
+      }
+      product.quantity--;
+    }
+  });
+}
+/* Create a function named removeProductFromCart that takes in the productId as an argument
+  - removeProductFromCart should get the correct product based on the productId
+  - removeProductFromCart should update the product quantity to 0
+  - removeProductFromCart should remove the product from the cart
+*/
 function removeProductFromCart(productId) {
-  const index = cart.findIndex((item) => item.productId === productId);
-
-  if (index !== -1) {
-    // Update the product quantity to 0
-    cart[index].quantity = 0;
-    // Remove the product from the cart
-    cart.splice(index, 1);
-  } else {
-    console.error('Product not found in the cart');
-  }
+  cart.forEach((product, index) => {
+    if (product.productId === productId) {
+      product.quantity--;
+      cart.splice(index, 1);
+    }
+  });
 }
+// remove Product from Cart - productId
+// remove Product from cart - update product quantity to 0
+// remove Product from Cart - remove product from cart
 
-// Create a function named cartTotal
+/* Create a function named cartTotal that has no parameters
+  - cartTotal should iterate through the cart to get the total of all products
+  - cartTotal should return the sum of the products in the cart
+*/
 function cartTotal() {
-  // Iterate through the cart to get the total cost of all products
-  return cart.reduce((total, item) => total + item.price * item.quantity, 0);
+  let total = 0;
+  cart.forEach((product) => {
+    total += product.price * product.quantity;
+  });
+  return total;
 }
-
-// Create a function named emptyCart
+/* Create a function called emptyCart that empties the products from the cart */
 function emptyCart() {
-  // Empty the products from the cart
-  cart.length = 0;
+  cart.forEach((product) => {
+    product.quantity = 0;
+  });
+  cart.splice(0, cart.length);
 }
-
-// Create a function named pay
+/* Create a function named pay that takes in an amount as an argument
+  - pay will return a negative number if there is a remaining balance
+  - pay will return a positive number if money should be returned to customer
+*/
+let balance = 0;
 function pay(amount) {
-  const total = cartTotal();
-  const remainingBalance = amount - total;
-  
-  // Update the global totalPaid variable
-  totalPaid += amount;
-  const remainingBalance = totalPaid - cartTotal();;
-
-  if (remainingBalance >= 0) {
-    // Update the global totalPaid variable
-    totalPaid += amount;
-
-    // Return the remaining balance
-    return remainingBalance;
-  } else {
-    // Handle the case where the customer has overpaid
-    console.error('Invalid payment amount. Customer has overpaid.');
-    return -1; // You can choose a different indicator for overpayment if needed
-  }
+  balance += amount;
+  return balance - cartTotal();
 }
+/* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
+// bonus comes here ;)
 
-// Uncomment the following line if completing the currency converter bonus
-// function currency
-
+/* The following is for running unit tests. 
+   To fully complete this project, it is expected that all tests pass.
+   Run the following command in terminal to run tests
+   npm run test
+*/
 
 module.exports = {
-   products,
-   cart,
-   addProductToCart,
-   increaseQuantity,
-   decreaseQuantity,
-   removeProductFromCart,
-   cartTotal,
-   pay, 
-   emptyCart,
-   totalPaid, // Add the totalPaid variable to the exported module
-   // currency
-}
+  products,
+  cart,
+  addProductToCart,
+  increaseQuantity,
+  decreaseQuantity,
+  removeProductFromCart,
+  cartTotal,
+  pay,
+  emptyCart,
+  /* Uncomment the following line if completing the currency converter bonus */
+  // currency
+};
